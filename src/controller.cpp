73413d336e2aa1e59053bafd4c2887a2ca382f2f
qwerzl <me@qwerzl.me>
2024-08-18 12:58:58 +0800

    fix: `INPUT_PULLUP` for arcade buttons

--- src/controller.cpp
+++ src/controller.cpp
@@ -1,7 +1,7 @@
 #include "Arduino.h"
 
 #define WIFI_SSID "ASUS"
-#define WIFI_PASSWORD "1360***8441"
+#define WIFI_PASSWORD "13601748441"
 #define MQTT_SERVER "192.168.50.199"
 #define JOYSTICK_POSITIVE_X_PIN D2
 #define JOYSTICK_POSITIVE_Y_PIN D3
@@ -34,6 +34,8 @@ void setup() {
     pinMode(JOYSTICK_NEGATIVE_Y_PIN, INPUT_PULLUP);
     pinMode(JOYSTICK_POSITIVE_X_PIN, INPUT_PULLUP);
     pinMode(JOYSTICK_POSITIVE_Y_PIN, INPUT_PULLUP);
+    pinMode(EM_BUTTON_PIN, INPUT_PULLUP);
+    pinMode(Z_BUTTTON_PIN, INPUT_PULLUP);
 }
 
 void loop() {
@@ -48,44 +50,53 @@ void loop() {
     int joystick_negative_x = digitalRead(JOYSTICK_NEGATIVE_X_PIN);
     int joystick_negative_y = digitalRead(JOYSTICK_NEGATIVE_Y_PIN);
     int z_button = digitalRead(Z_BUTTTON_PIN);
-    int em_button = digitalRead(EM_BUTTON_PIN);
+//    int em_button = digitalRead(EM_BUTTON_PIN);
 
     if (joystick_positive_x == LOW && currentDir != XPositive) {
         Serial0.println(XPositive);
+        Serial.println(XPositive);
         currentDir = XPositive;
     }
     else if (joystick_positive_y == LOW && currentDir != YPositive) {
         Serial0.println(YPositive);
+        Serial.println(YPositive);
         currentDir = YPositive;
     }
     else if (joystick_negative_x == LOW && currentDir != XNegative) {
         Serial0.println(XNegative);
+        Serial.println(XNegative);
         currentDir = XNegative;
     }
     else if (joystick_negative_y == LOW && currentDir != YNegative) {
         Serial0.println(YNegative);
+        Serial.println(YNegative);
         currentDir = YNegative;
     }
     else if ((joystick_positive_x + joystick_negative_x + joystick_positive_y + joystick_negative_y) == 4 && currentDir != Still) {
         Serial0.println(Still);
+        Serial.println(Still);
         currentDir = Still;
     }
 
     if (z_button == LOW && !z_button_pressed) {
         Serial0.println(Z_ON);
+        Serial.println(Z_ON);
         z_button_pressed = true;
     } else if (z_button == HIGH && z_button_pressed) {
         Serial0.println(Z_STILL);
+        Serial.println(Z_STILL);
         z_button_pressed = false;
     }
 
-    if (em_button == LOW && !em_button_pressed) {
-        Serial0.println(EM_ON);
-        z_button_pressed = true;
-    } else if (em_button == HIGH && em_button_pressed) {
-        Serial0.println(EM_OFF);
-        z_button_pressed = false;
-    }
+//    if (em_button == LOW && !em_button_pressed) {
+//        Serial0.println(EM_ON);
+//        Serial.println(EM_ON);
+//        z_button_pressed = true;
+//    } else if (em_button == HIGH && em_button_pressed) {
+//        Serial0.println(EM_OFF);
+//        Serial.println(EM_ON);
+//        z_button_pressed = false;
+//    }
 
     delay(50);
 }
